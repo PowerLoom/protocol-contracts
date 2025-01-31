ef74147c57300807ae34871a4b6d713ab59c00d2
Akshay Dahiya <akshay@powerloom.io>
2025-01-31 16:06:31 +0530

    chore: ignoring files in default hardhat coverage command now

--- hardhat/.solcover.js
+++ hardhat/.solcover.js
@@ -1,9 +1,8 @@
-//skip files
 module.exports = {
   skipFiles: [
     'TestArtifacts/DataMarketTestUpgrade.sol',
     'TestArtifacts/ProtocolStateV2.sol',
     'TestArtifacts/ProtocolStateV3Broken.sol',
     'TestArtifacts/PowerloomNodesUpgrade.sol'
-  ],
+  ]
 };
